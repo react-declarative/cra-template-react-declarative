6ceb4635ec84a371deb253971311d7b6923d8ddd
Petr Tripolsky <tripolskypetr@gmail.com>
2022-09-26 23:58:12 +0300

    version-inc

--- template/src/hooks/useLoader.tsx
+++ template/src/hooks/useLoader.tsx
@@ -1,22 +1,45 @@
-import { makeAutoObservable } from 'mobx';
-import { createProvider } from 'react-declarative';
+import { useCallback, useMemo } from 'react';
 
-export const [LoaderProvider, useLoader] = createProvider(() => new class {
-    
-    _loader = 0;
+import { createProvider, useModel, useActualValue } from 'react-declarative';
 
-    constructor() {
-        makeAutoObservable(this);
-    };
+interface IState {
+    loader: number;
+}
 
-    get loader() {
-        return !!this._loader;
-    };
+interface IContext {
+    loader: boolean;
+    setLoader: (loader: boolean) => void;
+}
 
-    setLoader = (loader: boolean) => {
-        this._loader = Math.max(this._loader + (loader ? 1 : -1), 0)
-    };
+export const [LoaderProviderCtx, useLoader] = createProvider<IContext>();
 
-});
+export const LoaderProvider = ({
+    children,
+}: {
+    children: React.ReactNode;
+}) => {
+    const model = useModel<IState>({
+        initialValue: () => ({
+            loader: 0,
+        }),
+        debounce: 25,
+    });
+    const model$ = useActualValue(model);
+    const setLoader = useCallback((loader: boolean) => {
+        const { current: model } = model$;
+        model.setData({
+            loader: Math.max(model.data.loader + (loader ? 1 : -1), 0),
+        });
+    }, [model$]);
+    const payload = useMemo((): IContext => ({
+        setLoader,
+        loader: !!model.data.loader,
+    }), [model.data.loader, setLoader]);
+    return (
+        <LoaderProviderCtx payload={payload}>
+            {children}
+        </LoaderProviderCtx>
+    );
+};
 
 export default useLoader;
